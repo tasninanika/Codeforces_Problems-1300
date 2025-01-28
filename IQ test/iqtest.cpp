5619f748183ed77ef232223b32ed5398cd9827a9
anikatasnin <jarintasnin09@gmail.com>
2025-01-28 10:50:18 +0600

    Added IQ test

--- IQ test/iqtest.cpp
+++ IQ test/iqtest.cpp
@@ -9,8 +9,15 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> arr[i];
 
-        if(arr[i] % 2 == 0)
+        if(arr[i] % 2 == 0){
+            c++;
+        }
+        else{
+            c2++;
+        }
     }
+
+
 
     return 0;
 }
