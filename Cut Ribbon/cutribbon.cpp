baba253d3629db6e224ed9517eb71aa825a96a5d
anikatasnin <jarintasnin09@gmail.com>
2025-02-27 13:42:53 +0600

    added Cut Ribbon

--- Cut Ribbon/cutribbon.cpp
+++ Cut Ribbon/cutribbon.cpp
@@ -1,10 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main(){
-
+int main() {
     int n, a, b, c;
     cin >> n >> a >> b >> c;
 
+    int maxPieces = 0;
+
+    for (int i = 0; i * a <= n; i++) {
+        for (int j = 0; i * a + j * b <= n; j++) {
+            int remaining = n - (i * a + j * b);
+            if (remaining % c == 0) {
+                int z = remaining / c;
+                maxPieces = max(maxPieces, i + j + z);
+            }
+        }
+    }
+
+    cout << maxPieces << endl;
     return 0;
 }
