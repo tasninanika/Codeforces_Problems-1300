4525a02f0099ef7fb0a795ed9853178283e7d9ae
anikatasnin <jarintasnin09@gmail.com>
2025-05-03 09:35:22 +0600

    added Sort the Array

--- Sort the Array/sortTheArray.cpp
+++ Sort the Array/sortTheArray.cpp
@@ -1,31 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n;
     cin >> n;
 
-    vector<int> a(n);
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-    }
+    vector<int> a(n), sorted_a(n);
 
-    int l = 0, r = n - 1;
-
-    while(l < n - 1 && a[l] <= a[l + 1]){
-        l++;
-    }
-    if(l == n - 1){
-        cout << "yes" << endl;
-        cout << "1 1" << endl;
-        return 0;
-    }
-
-    while(r > 0 && a[r - 1]){
-        r--;
-    }
-
-    reverse(a.begin(), a.end() + r + 1);
 
     return 0;
 }
