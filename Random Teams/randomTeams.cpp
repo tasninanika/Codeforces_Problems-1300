d555f7cfc229367a20fbaa77681f1b159db5b892
anikatasnin <jarintasnin09@gmail.com>
2025-05-20 11:36:40 +0600

    added Random Teams

--- Random Teams/randomTeams.cpp
+++ Random Teams/randomTeams.cpp
@@ -6,7 +6,8 @@ int main(){
     cin >> n >> m;
 
     long long max_friends = (n - m + 1) * (n - m) / 2;
-    long long b = n / m, c = n % m;
+    long long b = n / m;
+    long long c = n % m;
     long long min_friends = (m - c) * (b * (b - 1) / 2) + c * (b * (b + 1) / 2);
 
     cout << min_friends << " " << max_friends << endl;
