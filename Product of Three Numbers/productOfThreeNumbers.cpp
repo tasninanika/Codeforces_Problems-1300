0802297e85e1e26f929a0111f6d4be72046fac36
anikatasnin <jarintasnin09@gmail.com>
2025-05-20 11:18:25 +0600

    added Product of Three Numbers

--- Product of Three Numbers/productOfThreeNumbers.cpp
+++ Product of Three Numbers/productOfThreeNumbers.cpp
@@ -1,41 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int t;
     cin >> t;
 
-    while(t--){
-        int n;
+    while (t--) {
+        long long n;
         cin >> n;
 
-        int a = -1, b = -1;
+        long long factors[2];
+        int count = 0;
 
-        for(int i = 2; i * i <= n; i++){
-            if(n % i == 0){
-                a = i;
+        for (long long i = 2; i * i < n && count < 2; i++) {
+            if (n % i == 0) {
+                factors[count++] = i;
                 n /= i;
-                break;
             }
         }
 
-        for(int i = a + 1; i * i <= n; i++){
-            if(n % i == 0){
-                b = i;
-                n /= i;
-                break;
-            }
-        }
-
-        if(a != -1 && b != -1 && n > 1 && n != a && n != b){
-            cout << "YES" << endl;
-            cout << a << " " << b << " " << n << endl;
-        }
-        else{
+        if (count != 2 || n == factors[0] || n == factors[1] || n == 1) {
             cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+            cout << factors[0] << " " << factors[1] << " " << n << endl;
         }
     }
-
 
     return 0;
 }
