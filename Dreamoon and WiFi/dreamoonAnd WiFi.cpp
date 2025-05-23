9ccfece4149a088b1af92f640fd2c053b84d3b4f
anikatasnin <jarintasnin09@gmail.com>
2025-05-23 18:35:23 +0600

    added Dreamoon and WiFi

--- Dreamoon and WiFi/dreamoonAnd WiFi.cpp
+++ Dreamoon and WiFi/dreamoonAnd WiFi.cpp
@@ -5,7 +5,15 @@ int main(){
     string s1, s2;
     cin >> s1 >> s2;
 
-
+    int x = 0;
+    for (int i = 0; i < (int)s.size(); i++) {
+        if (s[i] == '+'){
+            x++;
+        }
+        else{
+            x--;
+        }
+    }
 
     return 0;
 }
