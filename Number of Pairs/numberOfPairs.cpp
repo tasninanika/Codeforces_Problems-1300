19503e0663e5938fff8d6c553c89ff7be8e2a908
anikatasnin <jarintasnin09@gmail.com>
2025-06-20 11:59:46 +0600

    added Number of Pairs

--- Number of Pairs/numberOfPairs.cpp
+++ Number of Pairs/numberOfPairs.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, l, r;
+        cin >> n >> l >> r;
+
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+
+        sort(v.begin(), v.end());
+
+        long long c = 0;
+
+        for (int i = 0; i < n; i++) {
+            int left = lower_bound(v.begin() + i + 1, v.end(), l - v[i]) - v.begin();
+            int right = upper_bound(v.begin() + i + 1, v.end(), r - v[i]) - v.begin();
+            count += (right - left);
+        }
+
+        cout << c << endl;
+    }
+
+    return 0;
+}
 
