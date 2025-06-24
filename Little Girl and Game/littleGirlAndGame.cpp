2c991dfdf5115d00a48efb5d02bed8fc490fd4f3
anikatasnin <jarintasnin09@gmail.com>
2025-06-24 19:49:08 +0600

    added Little Girl and Game

--- Little Girl and Game/littleGirlAndGame.cpp
+++ Little Girl and Game/littleGirlAndGame.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string s;
+    cin >> s;
+
+    int cnt[26] = {0};
+
+    for(int i = 0; i < s.size(); i++){
+        cnt[s[i] - 'a']++;
+    }
+
+    int odd = 0;
+
+    for(int i = 0; i < 26; i++){
+        if(cnt[i] % 2 == 1){
+            odd++;
+        }
+    }
+
+    if(odd == 0 || odd % 2 == 1){
+        cout << "First" << endl;
+    }
+    else{
+        cout << "Second" << endl;
+    }
+
+    return 0;
+}
