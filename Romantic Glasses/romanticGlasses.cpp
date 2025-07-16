34832b601f043519e7c535c2685999974fda39d9
anikatasnin <jarintasnin09@gmail.com>
2025-07-16 22:38:43 +0600

    added Romantic Glasses

--- Romantic Glasses/romanticGlasses.cpp
+++ Romantic Glasses/romanticGlasses.cpp
@@ -8,6 +8,14 @@ int main(){
     while(t--){
         int n;
         cin >> n;
+
+        map<long long, bool> seen;
+        seen[0] = true;
+
+        long long diff = 0;
+        bool found = false;
+
+
     }
 
 
