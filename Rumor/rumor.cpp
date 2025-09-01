2c6c39d1dfef498d013df406e5cfcdc34564cc38
anikatasnin <jarintasnin09@gmail.com>
2025-09-01 18:37:09 +0600

    added Rumor

--- Rumor/rumor.cpp
+++ Rumor/rumor.cpp
@@ -1,33 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+int solve(){
+	ll n,m;
+	cin >> n >> m;
+
+	ll a[n];
+	for(ll i = 0; i < n; i++)
+        cin >> a[i];
+
+	for(ll i = 0; i < m; i++){
+		ll x, y;
+        cin >> x >> y;
+		x--;
+		y--;
+		ll t = min(a[x],a[y]);
+		a[x] = t;
+		a[y] = 0;
+	}
+	ll res = 0;
+	for(ll i = 0; i < n; i++)
+        res += a[i];
+
+	cout << res << endl;
+
+	return 0;
+}
 
 int main(){
-    long long n, m;
-    cin >> n >> m;
+	ll t=1;
 
-    vector<long long> a(n);
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-    }
+	while(t--)
+        solve();
 
-    for(int i = 0; i < m; i++){
-        long long x, y;
-        cin >> x >> y;
-        x--;
-        y--;
-        long long t = min(a[x], a[y]);
-        a[x] = t;
-        a[y] = 0;
-    }
-
-    long long res = 0;
-    for(int i = 0; i < n; i++){
-        res += a[i];
-    }
-
-    cout << res << endl;
-
-
-
-    return 0;
+	return 0;
 }
+
