d6b1d26a76d4e29f80e3d53d16d11cf36c6513a4
anikatasnin <jarintasnin09@gmail.com>
2025-09-10 23:28:29 +0600

    added Strange Birthday Party

--- Strange Birthday Party/strangeBirthdayParty.cpp
+++ Strange Birthday Party/strangeBirthdayParty.cpp
@@ -1,1 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+
+        vector<int> k(n);
+        for(int i = 0; i < n; ++i)
+            cin >> k[i];
+
+        vector<ll> c(m+1);
+        for(int j = 1; j <= m; ++j)
+            cin >> c[j];
+
+        sort(k.begin(), k.end(), greater<int>());
+
+        ll ans = 0;
+        int p = 1;
+        for(int i = 0; i < n; ++i){
+            int ki = k[i];
+
+            if(p <= m && p <= ki && c[p] < c[ki]){
+                ans += c[p];
+                ++p;
+            }
+            else {
+                ans += c[ki];
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
 
