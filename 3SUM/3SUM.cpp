e7d073d8cbd638e6cb519c24235b188eefda0790
anikatasnin <jarintasnin09@gmail.com>
2025-09-11 22:47:54 +0600

    added 3SUM

--- 3SUM/3SUM.cpp
+++ 3SUM/3SUM.cpp
@@ -1,51 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
     cin >> t;
-
-    while(t--){
+    while (t--) {
         int n;
         cin >> n;
-
         vector<int> cnt[10];
-        for(int i = 0; i < n; i++){
+
+        for (int i = 0; i < n; i++) {
             long long x;
             cin >> x;
-
             int r = x % 10;
-            if((int)cnt[r].size() < 3){
+            if ((int)cnt[r].size() < 3) {
                 cnt[r].push_back(r);
             }
+        }
 
-            vector<int> vals;
-            for (int d = 0; d < 10; d++) {
-                for (int j = 0; j < (int)cnt[d].size(); j++) {
-                    vals.push_back(cnt[d][j]);
-                }
+        vector<int> vals;
+        for (int d = 0; d < 10; d++) {
+            for (int j = 0; j < (int)cnt[d].size(); j++) {
+                vals.push_back(cnt[d][j]);
             }
+        }
 
-            int sz = vals.size();
-            int found = 0;
+        int sz = vals.size();
+        int found = 0;
 
-            for (int i = 0; i < sz; i++) {
-                for (int j = i + 1; j < sz; j++) {
-                    for (int k = j + 1; k < sz; k++) {
-                        int sum = vals[i] + vals[j] + vals[k];
-                        if (sum % 10 == 3) {
-                            cout << "YES" << endl;
-                            found = 1;
-                            break;
-                        }
+        for (int i = 0; i < sz; i++) {
+            for (int j = i + 1; j < sz; j++) {
+                for (int k = j + 1; k < sz; k++) {
+                    int sum = vals[i] + vals[j] + vals[k];
+                    if (sum % 10 == 3) {
+                        cout << "YES" << endl;
+                        found = 1;
+                        break;
                     }
-                    if (found) break;
                 }
                 if (found) break;
             }
-            if (!found) {
-                cout << "NO" << endl;
-            }
+            if (found) break;
+        }
+
+        if (!found) {
+            cout << "NO" << endl;
         }
     }
 
