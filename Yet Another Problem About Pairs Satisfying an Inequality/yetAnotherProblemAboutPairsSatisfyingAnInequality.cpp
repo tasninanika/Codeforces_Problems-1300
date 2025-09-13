214d1524e8e107452dfe4f261200c1b641de5f28
anikatasnin <jarintasnin09@gmail.com>
2025-09-13 15:57:31 +0600

    added Yet Another Problem About Pairs Satisfying an Inequality

--- Yet Another Problem About Pairs Satisfying an Inequality/yetAnotherProblemAboutPairsSatisfyingAnInequality.cpp
+++ Yet Another Problem About Pairs Satisfying an Inequality/yetAnotherProblemAboutPairsSatisfyingAnInequality.cpp
@@ -28,7 +28,8 @@ int main(){
                 int right = j - 1;
 
                 if(left <= right){
-
+                    int cnt = upper_bound(valid.begin(), valid.end(), right)
+                              - lower_bound(valid.begin(), valid.end(), left);
                 }
             }
         }
