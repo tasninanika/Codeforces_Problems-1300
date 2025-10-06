82f92f3afe169e00a29b96ece48fa592f754cea9
anikatasnin <jarintasnin09@gmail.com>
2025-10-06 18:39:31 +0600

    added Strong Vertices

--- Strong Vertices/strongVertices.cpp
+++ Strong Vertices/strongVertices.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int> a(n), b(n), diff(n);
+        int maxDiff = INT_MIN;
+
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        for(int i = 0; i < n; i++){
+            cin >> b[i];
+
+            diff[i] = a[i] - b[i];
+            maxDiff = max(maxDiff, diff[i]);
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < n; i++){
+            if(diff[i] == maxDiff) {
+                ans.push_back(i + 1);
+            }
+        }
+        cout << ans.size() << endl;
+
+        for(int i = 0; i < ans.size(); i++){
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+}
